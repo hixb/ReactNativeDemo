3725c3de0709944ca83f803ca43cb379094e98c5
hixb <Hello16369@163.com>
2023-03-18 15:13:23 +0800

    style: layout information callback

--- src/components/ViewDemo.js
+++ src/components/ViewDemo.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 
 export default () => {
+  const [height, setHeight] = useState(100);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHeight(200);
+    }, 2000);
+  }, []);
+
   return (
     <View style={styles.root}>
-      <View style={[styles.subView]} />
+      <View
+        style={[styles.subView, {height}]}
+        onLayout={e => {
+          console.log('subView onLayout', e.nativeEvent.layout.height);
+        }}
+      />
     </View>
   );
 };
@@ -15,14 +28,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#ccc',
     height: '100%',
     width: '100%',
-    alignItems: 'center',
   },
   subView: {
     width: 100,
-    height: 100,
     backgroundColor: '#222',
-    position: 'absolute',
-    top: 30,
-    left: 20,
   },
 });
