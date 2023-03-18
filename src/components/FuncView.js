2a9de39be00123a5d2387a3414312bdfce9e840e
hixb <Hello16369@163.com>
2023-03-18 14:08:24 +0800

    feat: conditional rendering

--- src/components/FuncView.js
+++ src/components/FuncView.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, ScrollView} from 'react-native';
 import InfoCard from './InfoCard';
 
 export default () => {
-  const getLevelView = () => <Text style={{color: 'red'}}>{'level: 3'}</Text>;
+  const [levelUp, setLevelUp] = useState(false);
+  const [scrollValue, setScrollValue] = useState(['AAA', 'BBB', 'DDD']);
 
-  const getChildrenView = () => (
-    <Text style={{color: 'blue'}}>{'children'}</Text>
-  );
+  useEffect(() => {
+    setTimeout(() => {
+      setLevelUp(true);
+      setScrollValue(['SSS', 'AAADE']);
+    }, 2000);
+  }, []);
+
+  const getLevelView = () =>
+    levelUp ? (
+      <Text style={{color: 'red'}}>{'level: up'}</Text>
+    ) : (
+      <Text style={{color: 'yellow'}}>{'level: down'}</Text>
+    );
+
+  const getListView = () => {
+    const viewList = [];
+    for (let i = 0; i < 5; i++) {
+      viewList.push(<Text key={i}>{i + 1}</Text>);
+    }
+    return viewList;
+  };
 
   return (
     <View style={{width: '100%', height: '100%', backgroundColor: '#f5f5f5'}}>
       <InfoCard name="John" age={20} levelView={getLevelView()}>
-        {getChildrenView()}
+        <Text style={{color: 'blue'}}>{'children'}</Text>
       </InfoCard>
+      {/*<ScrollView>*/}
+      {/*  {scrollValue.map(item => (*/}
+      {/*    <Text key={item}>{item}</Text>*/}
+      {/*  ))}*/}
+      {/*</ScrollView>*/}
+      <ScrollView>{getListView()}</ScrollView>
     </View>
   );
 };
